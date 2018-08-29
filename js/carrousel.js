7df71c610ce6413cecd00991b9dd0171cf865ed5
CASTELLINI <dcastellini@corp.aa2000.com.ar>
2018-08-29 16:14:08 -0300

    Decimo commit

--- js/carrousel.js
+++ js/carrousel.js
@@ -20,3 +20,26 @@ $(document).ready(function(){
             }
         });
 });
+
+$(document).ready(function(){
+        $("#carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            autoplay:true,
+            responsive:{
+                0:{
+                    items:2,
+                    mouseDrag: false,
+                    touchDrag: true
+                },
+                600:{
+                    items:3,
+                    mouseDrag: false,
+                    touchDrag: true
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+});
