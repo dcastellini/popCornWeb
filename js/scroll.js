fc1271ce9e4b0fb79cc5dafcfd250bc3da7673ba
Damian <dcastellini89@gmail.com>
2018-11-19 21:18:50 -0300

    Nueva

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(function (){
-      $(window).scroll(function(){
-         if ($(this).scrollTop() > 10) {
-          $('.navscroll').addClass("black");
-         } else {
-          $(".navscroll").removeClass("black");
-         }
-      });
-  });
